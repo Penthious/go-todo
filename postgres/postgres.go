b3c71961f4915a1cc19d9d6fc17270f67309547f
Tomas <tleffew1994@gmail.com>
2019-12-22 18:00:42 -0700

    Added in logging to for sql querys

--- postgres/postgres.go
+++ postgres/postgres.go
@@ -1,12 +1,26 @@
 package postgres
 
 import (
+	"context"
+	"fmt"
 	"github.com/go-pg/pg/v9"
 	_ "github.com/lib/pq"
 )
 
+type dbLogger struct{}
+
+func (d dbLogger) BeforeQuery(c context.Context, q *pg.QueryEvent) (context.Context, error) {
+	return c, nil
+}
+
+func (d dbLogger) AfterQuery(c context.Context, q *pg.QueryEvent) error {
+	fmt.Println(q.FormattedQuery())
+	return nil
+}
+
 func New(opts *pg.Options) *pg.DB {
 	db := pg.Connect(opts)
+	db.AddQueryHook(dbLogger{})
 
 	return db
 }
